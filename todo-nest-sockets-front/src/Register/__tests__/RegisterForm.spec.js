f9c8def51a709333939e1a91cabb9e10b9bf5976
alexsoler <alejosod@gmail.com>
2021-01-31 14:56:23 -0600

    more test cases added

--- todo-nest-sockets-front/src/Register/__tests__/RegisterForm.spec.js
+++ todo-nest-sockets-front/src/Register/__tests__/RegisterForm.spec.js
@@ -4,7 +4,6 @@ import {
   cleanup,
   fireEvent,
   render,
-  screen,
 } from '@testing-library/react';
 import { RegisterForm } from '../Components';
 
@@ -21,6 +20,54 @@ describe('RegisterForm Component', () => {
     await findByTestId(`${blockName}--email_input`);
     await findByTestId(`${blockName}--password_input`);
     await findByTestId(`${blockName}--submit-button`);
+  });
+
+  it('typing lastName field should change its value', async () => {
+    const testValue = 'alejandro';
+
+    const { findByTestId } = render(<RegisterForm onSubmitRegisterForm={() => {}} />);
+
+    const lastNameField = await findByTestId(`${blockName}--lastName_input`);
+
+    await act(async () => {
+      await fireEvent.change(lastNameField, {
+        target: { value: testValue },
+      });
+    });
+
+    expect(lastNameField).toHaveValue(testValue);
+  });
+
+  it('typing email field should change its value', async () => {
+    const testValue = 'alejandro';
+
+    const { findByTestId } = render(<RegisterForm onSubmitRegisterForm={() => {}} />);
+
+    const emailField = await findByTestId(`${blockName}--email_input`);
+
+    await act(async () => {
+      await fireEvent.change(emailField, {
+        target: { value: testValue },
+      });
+    });
+
+    expect(emailField).toHaveValue(testValue);
+  });
+
+  it('typing password field should change its value', async () => {
+    const testValue = 'alejandro';
+
+    const { findByTestId } = render(<RegisterForm onSubmitRegisterForm={() => {}} />);
+
+    const passwordField = await findByTestId(`${blockName}--email_input`);
+
+    await act(async () => {
+      await fireEvent.change(passwordField, {
+        target: { value: testValue },
+      });
+    });
+
+    expect(passwordField).toHaveValue(testValue);
   });
 
   it('should not let you click the button if the form do not contains a firstName', async () => {
@@ -77,6 +124,30 @@ describe('RegisterForm Component', () => {
 
     const firstNameInput = await findByTestId(`${blockName}--firstName_input`);
     const lastNameInput = await findByTestId(`${blockName}--lastName_input`);
+    const passwordInput = await findByTestId(`${blockName}--password_input`);
+    const formButton = await findByTestId(`${blockName}--submit-button`);
+
+    await act(async () => {
+      await fireEvent.change(firstNameInput, {
+        target: { value: testValue },
+      });
+      await fireEvent.change(passwordInput, {
+        target: { value: testValue },
+      });
+      await fireEvent.change(lastNameInput, {
+        target: { value: testValue },
+      });
+    });
+
+    expect(formButton).toBeDisabled();
+  });
+
+  it('should not let you click the button if the form do not contains a password', async () => {
+    const testValue = 'test';
+    const { findByTestId } = await render(<RegisterForm onSubmitRegisterForm={() => {}} />);
+
+    const firstNameInput = await findByTestId(`${blockName}--firstName_input`);
+    const lastNameInput = await findByTestId(`${blockName}--lastName_input`);
     const emailInput = await findByTestId(`${blockName}--email_input`);
     const formButton = await findByTestId(`${blockName}--submit-button`);
 
@@ -95,19 +166,27 @@ describe('RegisterForm Component', () => {
     expect(formButton).toBeDisabled();
   });
 
-  it('typing firstname field should change its value', async () => {
-    const testValue = 'alejandro';
+  it('should not let you click the button if the form do not contains a password', async () => {
+    const testValue = 'test';
+    const { findByTestId } = await render(<RegisterForm onSubmitRegisterForm={() => {}} />);
 
-    const { findByTestId } = render(<RegisterForm onSubmitRegisterForm={() => {}} />);
-
-    const firstNameField = await findByTestId(`${blockName}--firstName_input`);
+    const firstNameInput = await findByTestId(`${blockName}--firstName_input`);
+    const lastNameInput = await findByTestId(`${blockName}--lastName_input`);
+    const emailInput = await findByTestId(`${blockName}--email_input`);
+    const formButton = await findByTestId(`${blockName}--submit-button`);
 
     await act(async () => {
-      await fireEvent.change(firstNameField, {
+      await fireEvent.change(firstNameInput, {
+        target: { value: testValue },
+      });
+      await fireEvent.change(emailInput, {
+        target: { value: testValue },
+      });
+      await fireEvent.change(lastNameInput, {
         target: { value: testValue },
       });
     });
 
-    expect(firstNameField).toHaveValue(testValue);
+    expect(formButton).toBeDisabled();
   });
 });
